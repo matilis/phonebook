2a8e52eb85449376f1c980b33a3660e0a30ae6c2
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-08-06 17:07:09 +0200

    [phonebook][fix] - background photo position

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import { AppBar } from 'components/AppBar';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import css from './Layout.module.css';
 
 export const Layout = () => {
   return (
     <div>
       <AppBar />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+      <div className={css.book}>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </div>
     </div>
   );
 };
