d826fc3becdba28a3552a7d1be3e87e684d4b3dd
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-08-07 14:37:49 +0200

    [phonebook][feat] -  add position login and register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,15 @@
 import { LoginForm } from 'components';
 import { Helmet } from 'react-helmet';
+import css from './Login.module.css';
 
 const Login = () => (
   <div>
     <Helmet>
       <title>Login</title>
     </Helmet>
+    <div className={css.homePage}>
+      <h1 className={css.homePage__title}>Phonebook</h1>
+    </div>
     <LoginForm />
   </div>
 );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,15 @@
 import { RegisterForm } from 'components';
 import { Helmet } from 'react-helmet';
+import css from './Register.module.css';
 
 const Register = () => (
   <div>
     <Helmet>
       <title>Register</title>
     </Helmet>
+    <div className={css.homePage}>
+      <h1 className={css.homePage__title}>Phonebook</h1>
+    </div>
     <RegisterForm />
   </div>
 );
