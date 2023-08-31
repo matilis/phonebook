5b45bb68f591070c5493d7fcd7bd21aea3f07e9e
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-08-31 19:11:01 +0200

    fix style

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,35 +1,3 @@
-// import css from './Navigation.module.css';
-// import homeSvg from 'images/icon-home.png';
-// import { useAuth } from 'hooks';
-// import { NavLink } from 'react-router-dom';
-// import { activeStyle } from 'styles';
-
-// export const Navigation = () => {
-//   const { isLoggedIn } = useAuth();
-
-//   return (
-//     <nav className={css.nav}>
-//       <NavLink
-//         style={({ isActive }) => activeStyle(isActive)}
-//         className={css.nav__link}
-//         to="/"
-//       >
-//         <img className={css.nav__icon} src={homeSvg} alt="home icon hause" />
-//         Home
-//       </NavLink>
-//       {isLoggedIn && (
-//         <NavLink
-//           style={({ isActive }) => activeStyle(isActive)}
-//           className={css.nav__link}
-//           to="/contacts"
-//         >
-//           Contacts
-//         </NavLink>
-//       )}
-//     </nav>
-//   );
-// };
-
 import css from './Navigation.module.css';
 import homeSvg from 'images/icon-home.png';
 import { useAuth } from 'hooks';
@@ -40,28 +8,24 @@ export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   const handleNavLinkClick = () => {
-    // Wibracja
     if ('vibrate' in navigator) {
-      navigator.vibrate(200); // Czas trwania wibracji w milisekundach
+      navigator.vibrate(200);
     }
   };
 
   return (
     <nav className={css.nav}>
       <NavLink
-        exact
         onClick={handleNavLinkClick}
         style={({ isActive }) => activeStyle(isActive)}
         className={css.nav__link}
         to="/"
       >
-        <img src={homeSvg} alt="Home" />
+        <img className={css.nav__icon} src={homeSvg} alt="home icon hause" />
         Home
       </NavLink>
-
       {isLoggedIn && (
         <NavLink
-          exact
           onClick={handleNavLinkClick}
           style={({ isActive }) => activeStyle(isActive)}
           className={css.nav__link}
